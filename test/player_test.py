cdc52da53213b55feb523a449609388f50f89dde
J155917 <J155917@tafe.wa.edu.au>
2025-02-27 14:17:26 +0800

    feat: add unit tests for Player class properties

--- test/player_test.py
+++ test/player_test.py
@@ -2,10 +2,10 @@ import unittest
 from app.player import Player
 
 class TestPlayer(unittest.TestCase):
-    def test_uid_method_return_uid_value(self):
+    def test_uid_property_returns_uid_value(self):
         player_1 = Player("1", "John")
         self.assertEqual(player_1.uid, "1")
 
-    def test_name_method_return_name_value(self):
+    def test_name_property_returns_name_value(self):
         player_1 = Player("1", "John")
         self.assertEqual(player_1.name, "John")
