819e29cfe0c8d2605a3fb22a171440c0318058cb
J155917 <J155917@tafe.wa.edu.au>
2025-02-28 14:36:03 +0800

    feat: add methods to add at tail, delete head, delete tail, and delete by key with testcases

--- app/player_list.py
+++ app/player_list.py
@@ -1,3 +1,9 @@
+from app.player_node import PlayerNode
+
+
+class EmptyListException(Exception):
+    pass
+
 class PlayerList:
     def __init__(self):
         self._head = None
@@ -17,11 +23,80 @@ class PlayerList:
         else:
             player_node.next_node = self._head
             self._head.prev_node = player_node
-            self._tail = self._head
+            # when there is only one node in the list
+            if not self._tail:
+                self._tail = self._head
             self._head = player_node
+
+    def add_at_tail(self, player_node) -> None:
+        if self._is_empty():
+            self._tail = player_node
+        else:
+            self._tail.next_node = player_node
+            player_node.prev_node = self._tail
+            # when there is only one node in the list
+            if not self._head:
+                self._head = self._tail
+            self._tail = player_node
+
+    def remove_at_head(self) -> None :
+        if self._is_empty():
+            raise EmptyListException("Player list is empty.")
+        # when there is only one node in the list
+        elif not self._tail:
+            self._head = None
+        # when there are 2 nodes in the list
+        elif self._head.next_node is self._tail:
+            self._tail.prev_node = None
+            self._tail.next_node = None
+            # when removing at head, we do not modify the tail
+            self._head = None
+        else:
+            new_head = self._head.next_node
+            new_head.prev_node = None
+            self._head = new_head
+
+    def remove_at_tail(self) -> None :
+        if self._is_empty():
+            raise EmptyListException("Player list is empty.")
+        # when there is only one node in the list
+        elif not self._head:
+            self._tail = None
+        # when there are 2 nodes in the list
+        elif self._tail.prev_node is self._head:
+            self._head.next_node = None
+            # when removing at tail, we do not modify the head
+            self._tail = None
+        else:
+            new_tail = self._tail.prev_node
+            new_tail.next_node = None
+            self._tail = new_tail
+
+    def remove_node_by_key(self, key: str) -> None:
+        player_node = self._get_player_node_by_key(key)
+        if self._head is player_node:
+            self.remove_at_head()
+        elif self._tail is player_node:
+            self.remove_at_tail()
+        else:
+            node_before = player_node.prev_node
+            node_after = player_node.next_node
+            node_before.next_node = node_after
+            node_after.prev_node = node_before
+
+    def _get_player_node_by_key(self, key) -> PlayerNode | None:
+        current_node = self._head
+        while current_node:
+            if key == current_node.key:
+                return current_node
+            else:
+                current_node = current_node.next_node
+        return None
 
     def _is_empty(self) -> bool:
         return self._head is None and self._tail is None
+
+
 
 
 
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from app.player import Player
-from app.player_list import PlayerList
+from app.player_list import PlayerList, EmptyListException
 from app.player_node import PlayerNode
 
 
@@ -15,12 +15,16 @@ class TestPlayerList(unittest.TestCase):
         self.player_2 = Player("2", "Mary")
         self.player_node_2 = PlayerNode(self.player_2)
 
+        self.player_3 = Player("3", "Bob")
+        self.player_node_3 = PlayerNode(self.player_3)
+
     def test_add_at_head_when_player_list_empty(self):
         self.player_list.add_at_head(self.player_node_1)
 
         self.assertEqual(self.player_list.head, self.player_node_1)
+        self.assertIsNone(self.player_list.tail)
 
-    def test_add_at_head_when_player_list_not_empty(self):
+    def test_add_at_head_when_player_list_has_one_node(self):
         self.player_list.add_at_head(self.player_node_1)
         self.player_list.add_at_head(self.player_node_2)
 
@@ -28,5 +32,137 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(self.player_list.tail, self.player_node_1)
         self.assertEqual(self.player_node_2.next_node, self.player_node_1)
         self.assertEqual(self.player_node_1.prev_node, self.player_node_2)
+
+    def test_add_at_head_when_player_list_has_more_than_one_node(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+        self.player_list.add_at_head(self.player_node_3)
+
+        self.assertEqual(self.player_list.head, self.player_node_3)
+        self.assertEqual(self.player_list.tail, self.player_node_1)
+        self.assertIsNone(self.player_list.head.prev_node)
+        self.assertIsNone(self.player_list.tail.next_node)
+        self.assertEqual(self.player_node_2.next_node, self.player_node_1)
+        self.assertEqual(self.player_node_2.prev_node, self.player_node_3)
+        self.assertEqual(self.player_node_1.prev_node, self.player_node_2)
+        self.assertEqual(self.player_node_3.next_node, self.player_node_2)
+
+    def test_add_at_tail_when_player_list_empty(self):
+        self.player_list.add_at_tail(self.player_node_1)
+
+        self.assertEqual(self.player_list.tail, self.player_node_1)
+        self.assertIsNone(self.player_list.head)
+
+    def test_add_at_tail_when_player_list_not_empty(self):
+        self.player_list.add_at_tail(self.player_node_1)
+        self.player_list.add_at_tail(self.player_node_2)
+
+        self.assertEqual(self.player_list.tail, self.player_node_2)
+        self.assertEqual(self.player_list.head, self.player_node_1)
+        self.assertEqual(self.player_node_2.prev_node, self.player_node_1)
+        self.assertEqual(self.player_node_1.next_node, self.player_node_2)
+
+    def test_remove_at_head_when_player_list_empty(self):
+        with self.assertRaises(EmptyListException) as ex:
+            self.player_list.remove_at_head()
+
+        self.assertEqual(type(ex.exception), EmptyListException)
+        self.assertEqual(ex.exception.args[0], "Player list is empty.")
+
+    def test_remove_at_head_when_player_list_has_one_player_node(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.remove_at_head()
+
+        self.assertIsNone(self.player_list.head)
+        self.assertIsNone(self.player_list.tail)
+
+    def test_remove_at_head_when_player_list_has_two_player_nodes(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+        self.player_list.remove_at_head()
+
+        self.assertEqual(self.player_list.tail, self.player_node_1)
+        self.assertIsNone(self.player_list.tail.prev_node)
+        self.assertIsNone(self.player_list.tail.next_node)
+        self.assertIsNone(self.player_list.head)
+
+    def test_remove_at_head_when_player_list_has_more_than_two_player_nodes(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+        self.player_list.add_at_head(self.player_node_3)
+        self.player_list.remove_at_head()
+
+        self.assertEqual(self.player_list.head, self.player_node_2)
+        self.assertEqual(self.player_list.tail, self.player_node_1)
+        self.assertIsNone(self.player_list.head.prev_node)
+
+    def test_remove_at_tail_when_player_list_empty(self):
+        with self.assertRaises(EmptyListException) as ex:
+            self.player_list.remove_at_tail()
+
+        self.assertEqual(type(ex.exception), EmptyListException)
+        self.assertEqual(ex.exception.args[0], "Player list is empty.")
+
+    def test_remove_at_tail_when_player_list_has_one_player_node(self):
+        self.player_list.add_at_tail(self.player_node_1)
+        self.player_list.remove_at_tail()
+
+        self.assertIsNone(self.player_list.head)
+        self.assertIsNone(self.player_list.tail)
+
+    def test_remove_at_tail_when_player_list_has_two_player_nodes(self):
+        self.player_list.add_at_tail(self.player_node_1)
+        self.player_list.add_at_tail(self.player_node_2)
+        self.player_list.remove_at_tail()
+
+        self.assertEqual(self.player_list.head, self.player_node_1)
+        self.assertIsNone(self.player_list.head.prev_node)
+        self.assertIsNone(self.player_list.head.next_node)
+        self.assertIsNone(self.player_list.tail)
+
+    def test_remove_at_tail_when_player_list_has_more_than_two_player_nodes(self):
+        self.player_list.add_at_tail(self.player_node_1)
+        self.player_list.add_at_tail(self.player_node_2)
+        self.player_list.add_at_tail(self.player_node_3)
+        self.player_list.remove_at_tail()
+
+        self.assertEqual(self.player_list.head, self.player_node_1)
+        self.assertEqual(self.player_list.tail, self.player_node_2)
+        self.assertIsNone(self.player_list.head.prev_node)
+        self.assertIsNone(self.player_list.tail.next_node)
+
+    def test_remove_node_by_key_when_node_is_the_head(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+        self.player_list.add_at_head(self.player_node_3)
+        self.player_list.remove_node_by_key("3")
+
+        self.assertEqual(self.player_list.head, self.player_node_2)
+        self.assertEqual(self.player_list.tail, self.player_node_1)
+        self.assertEqual(self.player_list.head.prev_node, None)
+
+    def test_remove_node_by_key_when_node_is_the_tail(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+        self.player_list.add_at_head(self.player_node_3)
+        self.player_list.remove_node_by_key("1")
+
+        self.assertEqual(self.player_list.head, self.player_node_3)
+        self.assertEqual(self.player_list.tail, self.player_node_2)
+        self.assertEqual(self.player_list.tail.next_node, None)
+
+    def test_remove_node_by_key_when_node_is_in_the_middle(self):
+        self.player_list.add_at_head(self.player_node_1)
+        self.player_list.add_at_head(self.player_node_2)
+        self.player_list.add_at_head(self.player_node_3)
+        self.player_list.remove_node_by_key("2")
+
+        self.assertEqual(self.player_list.head, self.player_node_3)
+        self.assertEqual(self.player_list.tail, self.player_node_1)
+        self.assertEqual(self.player_list.head.next_node, self.player_node_1)
+        self.assertEqual(self.player_list.tail.prev_node, self.player_node_3)
+
+
+
 
 
