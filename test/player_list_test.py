f273819c4543897950065ab5fbd6868517c1ffda
J155917 <J155917@tafe.wa.edu.au>
2025-03-06 15:25:56 +0800

    unittest for display

--- test/player_list_test.py
+++ test/player_list_test.py
@@ -162,7 +162,23 @@ class TestPlayerList(unittest.TestCase):
         self.assertEqual(self.player_list.head.next_node, self.player_node_1)
         self.assertEqual(self.player_list.tail.prev_node, self.player_node_3)
 
-    def test_display_when_forward_is_true(self):
+    def test_display_when_there_is_single_node_tail_and_forward_is_true(self):
+        self.player_list.add_at_tail(self.player_node_1)
+        player_nodes = self.player_list.display()
+
+        self.assertEqual(len(player_nodes), 1)
+        self.assertTrue(player_nodes[0].__contains__("1"))
+        self.assertTrue(player_nodes[0].__contains__("John"))
+
+    def test_display_when_there_is_single_node_head_and_forward_is_false(self):
+        self.player_list.add_at_head(self.player_node_1)
+        player_nodes = self.player_list.display(forward=False)
+
+        self.assertEqual(len(player_nodes), 1)
+        self.assertTrue(player_nodes[0].__contains__("1"))
+        self.assertTrue(player_nodes[0].__contains__("John"))
+
+    def test_display_when_there_are_more_than_single_node_and_forward_is_true(self):
         self.player_list.add_at_head(self.player_node_1)
         self.player_list.add_at_head(self.player_node_2)
         self.player_list.add_at_head(self.player_node_3)
@@ -174,7 +190,7 @@ class TestPlayerList(unittest.TestCase):
         self.assertTrue(player_nodes[1].__contains__("2"))
         self.assertTrue(player_nodes[2].__contains__("John"))
 
-    def test_display_when_forward_is_false(self):
+    def test_display_when_there_are_more_than_single_node_and_forward_is_false(self):
         self.player_list.add_at_tail(self.player_node_1)
         self.player_list.add_at_tail(self.player_node_2)
         self.player_list.add_at_tail(self.player_node_3)
